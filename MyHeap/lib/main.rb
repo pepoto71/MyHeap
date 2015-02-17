0c24d1469f0c247840267b5901dd6bd75a7330b8
pepoto71 <pepoto@gmail.com>
2015-02-17 22:23:59 +0200

    Add benchmark for dijkstra algorithm.

--- MyHeap/lib/main.rb
+++ MyHeap/lib/main.rb
@@ -1,10 +1,15 @@
 require './MyHeap'
 require './Vertex'
 require './UGraph'
+require 'benchmark'
 
 g=UGraph.new
 g.load_from("dijkstraData.txt")
-g.dijkstra(1)
+
+Benchmark.bmbm do |x|
+  x.report("dijkstra") { g.dijkstra(1)}  
+end
+
 vs=[10,20,30,40,50,60,70,80,90,100]
 result=[]
 vs.each do |v|
