5eb7906295e8f7bd4ca836c14768d4b8276e4170
pepoto71 <pepoto@gmail.com>
2015-02-17 15:13:41 +0200

    Add class and some methods to implementation

--- MyHeap/lib/main.rb
+++ MyHeap/lib/main.rb
@@ -1,6 +1,24 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-puts "Hello World"
-puts "Hello world"
+class MyHeap
+  attr_reader :heap
+  attr_accessor :size
+  def initialize
+    @heap=[]
+    @size=0
+  end
+  def pop
+    if @size>0
+      @heap.pop
+      #TODO after pop
+    else
+      nil
+    end
+  end
+  def push(obj)
+    #TODO push object into heap
+  end
+  def delete(obj)
+    if @heap.include?(obj)
+    #TODO delete object from heap
+    end
+  end
+end
